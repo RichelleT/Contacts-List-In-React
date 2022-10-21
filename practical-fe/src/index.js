515301a23ae02f44564a99d01800a162612167b0
Richelle Tan <r.kyj89@gmail.com>
2022-10-21 16:53:55 +0800

    initial app structure update

--- practical-fe/src/index.js
+++ practical-fe/src/index.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-function Hi() {
-  return <p>Hi.</p>;
-}
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-render(<Hi />, document.getElementById("app"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  //<React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  //</React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
