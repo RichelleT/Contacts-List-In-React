8b7a4b8c039500e19d4e90f1f26fbde799d4122f
Richelle Tan <r.kyj89@gmail.com>
2022-10-21 17:28:38 +0800

    add home.js and home to route

--- practical-fe/src/App.js
+++ practical-fe/src/App.js
@@ -1,9 +1,16 @@
 import * as React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./components/home/Home";
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
+  let navigate = useNavigate();
   return (
     <div className="App">
       {/* add routes, header, footer, & toastcontainer in here */}
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+      </Routes>
     </div>
   );
 }
