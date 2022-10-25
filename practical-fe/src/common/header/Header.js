ac6e9f66517613dd7785c5bcb4c94d25776c404c
RIchelleTan <wq.tan@felixdigital.biz>
2022-10-25 11:04:20 +0800

    header temp. done

--- practical-fe/src/common/header/Header.js
+++ practical-fe/src/common/header/Header.js
@@ -7,8 +7,15 @@ function Header() {
         className="navbar navbar-light"
         style={{ backgroundColor: "#e3f2fd" }}
       >
-        <div className="container-fluid">
-          <span className="navbar-brand mb-0 h1">Contacts</span>
+        <div className="container-fluid custNavbar">
+          <div className="spanLeft">
+            <a href="/" style={{ textDecoration: "none" }}>
+              <span className="navbar-brand mb-0 h1">Contacts</span>
+            </a>
+          </div>
+          <div className="spanRight">
+            <span>Hello, user.</span>
+          </div>
         </div>
       </nav>
     </div>
