7955e5a07a25f6312c14c81250e8f08b8fdfc556
Richelle Tan <r.kyj89@gmail.com>
2022-10-29 23:58:28 +0800

    add textinput.js; remove form code;

--- practical-fe/src/components/contacts/ContactsForm.js
+++ practical-fe/src/components/contacts/ContactsForm.js
@@ -1,12 +1,1 @@
-import React from "react";
-import Header from "../common/header/Header";
 
-function ContactsForm() {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
-}
-
-export default ContactsForm;
